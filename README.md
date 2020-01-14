745afb50161bfa807075bab51194c8c304f2ff4d
Ameya <ameyacraje@gmail.com>
2020-01-14 15:49:41 -0600

    readiness probe

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Kube
 
 This is a simple Python app using Flask that I'm creating to deploy to a Kubernetes cluster.
+
+Steps to run:
+1. Install Docker
+2. Build the Docker image using the Dockerfile
+    `docker build -f Dockerfile . -t <name-of-image>`
+3. Run the image created 
+    `docker run -d <name-of-image>`
--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from healthcheck import HealthCheck, EnvironmentDump
+# from healthcheck import HealthCheck, EnvironmentDump
 
 
 app = Flask(__name__)
@@ -11,6 +11,10 @@ def index():
 @app.route('/test', methods = ['GET'])
 def test():
     return render_template('test.html')
+
+@app.route('/healthz', methods = ['GET'])
+def healthcheck():
+    return "Healthy"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9000)
